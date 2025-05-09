82f25ca5251c0f7bdbc1dfe2389f0a785fe059d9
Nguyen_Trung_Duc <ducnt237314@sis.hust.edu.vn>
2025-05-09 12:57:03 +0700

    Add searchByID #2

--- Search/searchReport.h
+++ Search/searchReport.h
@@ -3,20 +3,6 @@
 
 #include "patient.h"  // Gồm định nghĩa struct Patient, Status, PatientNode
 
-// Cấu trúc nút cho cây nhị phân tìm kiếm (BST)
-typedef struct BSTNode {
-    char* id;               // ID của bệnh nhân (khóa)
-    Patient* patient;       // Con trỏ tới bệnh nhân
-    struct BSTNode* left;   // Con trỏ tới nhánh trái
-    struct BSTNode* right;  // Con trỏ tới nhánh phải
-} BSTNode;
-
-// Cấu trúc danh sách bệnh nhân
-typedef struct {
-    PatientNode* head;      // Đầu danh sách liên kết
-    int size;               // Số lượng bệnh nhân
-    BSTNode* bstRoot;       // Gốc của cây BST để tìm kiếm theo ID
-} PatientList;
 
 // Liệt kê toàn bộ bệnh nhân
 void listPatients(PatientList* list);
